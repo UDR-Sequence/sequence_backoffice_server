892b08adb90dc64e4de0f544de4b4ec6b7052edf
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-03 15:27:09 +0900

    feat: SystemLink entity 생성

---
+++ src/main/java/com/sequence/seuqnece_backoffice_server/system_link/entity/SystemLink.java
@@ -0,0 +1,28 @@
+package com.sequence.seuqnece_backoffice_server.system_link.entity;
+
+import com.sequence.seuqnece_backoffice_server.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "system_link")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+public class SystemLink extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // 예: "Grafana", "SonarQube", "phpMyAdmin"
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String url;
+
+    @Column(nullable = false)
+    private String description; // 설명: "서버 상태 모니터링", "정적 분석 도구"
+
+}
