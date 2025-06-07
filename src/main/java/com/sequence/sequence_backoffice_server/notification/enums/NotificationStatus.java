3860508a289c3a67e72fda9b30d39b64283701ff
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-07 18:07:48 +0900

    fix : notificationstatus 수정
    이미 isRead가 존재하므로 메세지 상태만 처리

--- src/main/java/com/sequence/sequence_backoffice_server/notification/enums/NotificationStatus.java
+++ src/main/java/com/sequence/sequence_backoffice_server/notification/enums/NotificationStatus.java
@@ -1,7 +1,6 @@
 package com.sequence.sequence_backoffice_server.notification.enums;
 
 public enum NotificationStatus {
-    UNREAD, // 읽지 않음
-    READ, // 읽음
     FAILED, // 알림 전송 실패
+    SUCCESS, // 알림 전송 성공
 }
