908b80152388ec0c1d034d9da0af6ec8773214ef
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-07 15:43:44 +0900

    fix : Component 추가. 전략이 컴포넌트로 등록되도록 수정

--- src/main/java/com/sequence/sequence_backoffice_server/db_pooling/service/CreateReportMessageHandler.java
+++ src/main/java/com/sequence/sequence_backoffice_server/db_pooling/service/CreateReportMessageHandler.java
@@ -2,7 +2,9 @@ package com.sequence.sequence_backoffice_server.db_pooling.service;
 
 import com.sequence.sequence_backoffice_server.global.enums.DBOpertation;
 import com.sequence.sequence_backoffice_server.db_pooling.dto.ReportEventMessage;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CreateReportMessageHandler implements ReportMessageHandlerStrategy{
     @Override
     public DBOpertation getSupportDBOpertation() {
--- src/main/java/com/sequence/sequence_backoffice_server/db_pooling/service/ReportHandlerContext.java
+++ src/main/java/com/sequence/sequence_backoffice_server/db_pooling/service/ReportHandlerContext.java
@@ -5,8 +5,10 @@ import com.sequence.sequence_backoffice_server.db_pooling.dto.ReportEventMessage
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ReportHandlerContext {
 
@@ -21,7 +23,8 @@ public class ReportHandlerContext {
         String op = ReportEventMessage.getPayload().getOp();
         ReportMessageHandlerStrategy reportMessageHandlerStrategy = reportStrategyMap.get(DBOpertation.from(op));
         if (reportMessageHandlerStrategy == null) {
-            return;
+            log.warn("지원하지 않는 DBOpertation: {}", op);
+            return; // 이건 사실 괜찮지만, Kafka 쪽 예외로 인식될 수도 있음
         }
 
         reportMessageHandlerStrategy.handle(ReportEventMessage);
